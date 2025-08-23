9848757a9e7d3e919511577c0e190d6a62c1bc62
juanledesmaa <umbranito@gmail.com>
2025-08-22 20:46:38 -0400

    Navbar should hopefully be fixed now

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './NavigationBar.scss';
 
 const NavigationBar = () => (
   <div className="navigationBar">
     <Navbar>
       <Container fluid>
-        <Row className="w-100 align-items-center justify-content-center">
-          <Col className="d-flex align-items-center justify-content-center px-2">
-            <Navbar.Brand as="div" style={{ cursor: 'pointer' }} onClick={() => window.scrollTo(0, 0)}>
-              <h1 className="brand-text m-0 text-center">SOMOS UNO - MISIÓN GUÁNICA</h1>
-            </Navbar.Brand>
-          </Col>
-        </Row>
+        <div className="w-100 d-flex align-items-center justify-content-center">
+          <h1 className="brand-text m-0 text-center" style={{ cursor: 'pointer' }} onClick={() => window.scrollTo(0, 0)}>
+            SOMOS UNO - MISIÓN GUÁNICA
+          </h1>
+        </div>
       </Container>
     </Navbar>
   </div>
